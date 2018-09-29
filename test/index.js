213e53a97232bbe181c1bfe9b9ff085e9216357c
Timur Manyanov <gtibalt@gmail.com>
2018-09-29 11:52:43 +0200

    Update tests

--- test/index.js
+++ test/index.js
@@ -1,25 +1,19 @@
 const { expect } = require('chai');
-import Game from '../src/game';
-import { weapons, weaponRules, outcomes } from '../src/const';
-
-const game = Game(weaponRules);
+import { roundResult } from '../src/game';
+import { weapons, outcomes } from '../src/const';
 
 describe('Game', () => {
-  it('should create the game', () => {
-    expect(game).to.have.property('version');
-  });
-
   describe('Win/lose logic', () => {
     it('Rock should beat Scissors', () => {
-      expect(game.roundResult(weapons.ROCK, weapons.SCISSORS)).to.equal(outcomes.WIN);
+      expect(roundResult(weapons.ROCK, weapons.SCISSORS)).to.equal(outcomes.WIN);
     });
 
     it('Paper should tie Paper', () => {
-      expect(game.roundResult(weapons.PAPER, weapons.PAPER)).to.equal(outcomes.TIE);
+      expect(roundResult(weapons.PAPER, weapons.PAPER)).to.equal(outcomes.TIE);
     });
 
     it('Paper should lose to Scissors', () => {
-      expect(game.roundResult(weapons.PAPER, weapons.SCISSORS)).to.equal(outcomes.LOSE);
+      expect(roundResult(weapons.PAPER, weapons.SCISSORS)).to.equal(outcomes.LOSE);
     });
   });
 });
